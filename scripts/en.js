8ffc9ed5ffe26213cf7d45c3818cfc5c07e8a633
Diogo Domene <ddomene@interaxa.com>
2023-02-28 15:49:15 -0300

    Adding english translation

--- scripts/en.js
+++ scripts/en.js
@@ -12,47 +12,47 @@
     documentation: 'Documentation',
     architecture: 'Architecture',
     profile: 'Profile',
-    profileDescription: `Habituado a desenvolver com precisão os softwares documentados, 
-    possuo criatividade e experiência para construir aplicações com a intenção de, 
-    prevenir excesso de manutenção, e fornecer agilidade no processo de desenvolvimento, 
-    com ferramentas como lint e task runners. Apto ao uso da metodologia SCRUM para melhor 
-    eficácia na agilidade, e melhor entendimento sobre os processos para que novas e melhores 
-    soluções possam ser descobertas a tempo. Sei como projetar aplicações que demandam um número 
-    elevado de requisições e serviços, e mante-las fortemente resilientes e disponíveis.`,
+    profileDescription: `I know how accurately developing documented software,
+    I have the creativity and experience to build applications with the intention of,
+    prevent over-maintenance, and provide agility in the development process,
+    with tools like lint and task runners. Able to use the SCRUM methodology to improve
+    effectiveness in agility, and better understanding of the processes so that new and better
+    solutions can be discovered in time. I know how to design applications that require a number
+    high number of requests and services, and keep them highly resilient and available.`,
     experience: 'Experience',
     occupation6: 'Software Engineer',
-    exp6: `A BTime oferece serviços focados em gerenciamento de equipes de campo, basicamente, uma 
-    plataforma de checklist.
-    No back end utilizamos a arquitetura de micro-serviços, cada serviço se comunicava com outro via seneca, 
-    um pacote específico para comunicação de micro-serviços em Node.js.
-    A persistência dos dados eram feitas através do PostgreSQL, tendo o Redis como recurso de cache.
-    Para manter a disponibilidade do back end, esses serviços eram gerenciados por containers dentro do Rancher.
-    No front end, utilizamos Angular 8, com o pattern Observable para usufruir de suas características de streaming 
-    para manter os checklists atualizados em tempo real.`,
+    exp6: `BTime offers services focused on managing field teams, basically a
+    checklist platform.
+    In the back end we used the micro-services architecture, each service communicated with the other via seneca,
+    a specific package for communicating microservices in Node.js.
+    Data persistence was done through PostgreSQL, with Redis as a cache resource.
+    To maintain backend availability, these services were managed by containers within Rancher.
+    On the front end, we use Angular 8, with the Observable pattern to take advantage of its streaming characteristics
+    to keep checklists updated in real time.`,
     occupation5: 'Software Engineer',
-    exp5: `Atividades: Atuo no projeto EPICALL, que visa oferecer 
-    ao cliente uma plataforma completa de comunicação, com recursos de telefonia (ligação de áudio via SIP), conferências e chat em tempo real, 
-    agendamento de conferências e outros recursos customizados. Utilizamos Node no backend, Angular 6 e React para o frontend.`,
+    exp5: `Activities: I work in the EPICALL project, which aims to offer
+    to the customer a complete communication platform, with telephony resources (audio connection via SIP), conferences and chat in real time,
+    conference scheduling and other custom features. We use Node on the backend, Angular 6 and React for the frontend.`,
     occupation4: 'Software Engineer',
-    exp4: `Atividades: Criamos uma exchange de criptomoedas, onde as transações foram feitas utilizando serviços da 
-    API BlockIO, que fornece a geração de carteiras virtuais, validação de transações e confirmações de transações já que esta, utiliza 
-    o modelo BLOCKCHAIN de validação. Como a necessidade da regra de negócio exigia uma aplicação real-time, utilizamos a stack NodeJS junto ao 
-    banco de dados Firebase, pois estes possuem características asíncronas que nos permite uma conexão não bloqueante. Para fins de produtividade, 
-    optamos o framework Loopback 3 para a exposição dos serviços via Rest, já que foi um produto que se demonstrou, na época, possuir vantagens 
-    em relação à esse ponto.`,
+    exp4: `Activities: We created a cryptocurrency exchange, where transactions were made using services from
+    BlockIO API, which provides virtual wallet generation, transaction validation and transaction confirmations as it uses
+    the validation BLOCKCHAIN ​​model. As the need for the business rule required a real-time application, we used the NodeJS stack alongside the
+    Firebase database, as these have asynchronous characteristics that allow us a non-blocking connection. For productivity purposes,
+    we opted for the Loopback 3 framework for exposing services via Rest, as it was a product that demonstrated to have advantages
+    in relation to this point.`,
     occupation3: 'Desenvolvedor Back-End',
-    exp3: `Atividades:  Estamos desenvolvendo um projeto de beckend de canais, criando uma arquitetura em 
-    Node.js, pois a aplicação atual tem problemas com disponibilidade e desempenho. A linguagem de 
-    programação usada é o TypeScript, e há muitas bibliotecas que estamos usando para acelerar o 
-    processo de desenvolvimento, como gulp.js (task runner), eslint, e etc. Para garantir uma 
-    disponibilidade efetiva, os servidores node.js são implantados em um container docker, e o Kubernets 
-    (gerenciador docker), faz o trabalho de gerenciar o docker, implantando rapidamente um novo container 
-    se algo der errado com o atual.
-    Todas as funcionalidades / API possuem um teste unitário, esses testes são feitos com mocha.js e chai.
-    Para fins de desempenho, utilizamos o redis, um armazenamento de estrutura de dados em memória, 
-    usado como banco de dados local, o que elimina a necessidade de comunicação contínua com o banco 
-    de dados no servidor.`,
-    occupation2: 'Consultor Sharepoint',
+    exp3: `Activities: We are developing a backend to condensate all the santander bank's channels responsible to client handle it's account, creating an architecture in
+    Node.js as the current application has issues with availability and performance. the language of
+    programming used is TypeScript, and there are many libraries that we are using to speed up the
+    development process, such as gulp.js (task runner), eslint, and so on. To guarantee a
+    effective availability, node.js servers are deployed in a docker container, and Kubernets
+    (docker manager), does the work of managing docker, quickly deploying a new container
+    if something goes wrong with the current one.
+    All features / API have a unit test, these tests are done with mocha.js and chai.
+    For performance purposes, we use redis, an in-memory data structure store,
+    used as a local database, which eliminates the need for continuous communication with the database
+    of data on the server.`,
+    occupation2: 'Sharepoint Developer',
     occupation1: 'Web Developer',
     education: 'Education',
     course: 'Computer Science',
